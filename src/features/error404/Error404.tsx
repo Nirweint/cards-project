8990daff681b408d48f7945d53e8b038f8e6d03e
Nirweint <nirweint_1@mail.ru>
2022-02-10 17:00:57 +0300

    error404 page done

--- src/features/error404/Error404.tsx
+++ src/features/error404/Error404.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import error404Image from '../../assets/images/404Error.png';
+import s from './Error404.module.css';
+import {NavLink} from "react-router-dom";
+import {PATH} from "../../app/routes/RoutesComponent";
 
 export const Error404 = () => {
     return (
-        <div>
-            Error404 page
+        <div className={s.wrapper}>
+            <div className={s.body}>
+                <img className={s.image} src={error404Image} alt="Error404"/>
+                <h3 className={s.link}>
+                    <NavLink to={PATH.PROFILE}>Return on profile</NavLink>
+                </h3>
+            </div>
         </div>
     );
 };
