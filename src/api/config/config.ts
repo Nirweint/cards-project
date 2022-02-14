4d43645fc5ed70c45790051b92902d796eec81ee
Nirweint <nirweint_1@mail.ru>
2022-02-14 11:35:14 +0300

    baseUrl change

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:7542/2.0/';
+const baseURL = 'http://localhost:7542/2.0/';
 
 export const instance = axios.create({
     baseURL,
