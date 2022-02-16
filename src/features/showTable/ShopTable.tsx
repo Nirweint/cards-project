2fd3dcc7b812a3f103630391c552813d8eceba7d
Sergo <bunubon@yandex.ru>
2022-02-16 10:46:51 +0100

    'tt'

--- src/features/showTable/ShopTable.tsx
+++ src/features/showTable/ShopTable.tsx
@@ -6,12 +6,13 @@ import {shopTableGet} from "../../state/middlewares/shopTable";
 import {RootStateType} from "../../state/store";
 import {AppStatusType} from "../../state/reducers/app";
 import {getPacksOfCards} from "../../state/middlewares/packs";
+import PackItam from "../../components/packItem/PackItam";
 
 
 const ShopTable = () => {
 
     const dispatch = useDispatch()
-    const cardPacks = useSelector((state: RootStateType) => state.cards.cardPack.cards)
+    const cardPacks = useSelector((state: RootStateType) => state.packs.pack.cardPacks)
 
     useEffect(() => {
         dispatch(getPacksOfCards())
@@ -23,14 +24,20 @@ const ShopTable = () => {
         <div>
             <h2>Table</h2>
             <div className={show_Table.container}>
-                <div>Product Name</div>
-                <div>price</div>
+                <div>Name</div>
+                <div>Cards Count</div>
+                <div>Update</div>
+                <div>URL</div>
                 <Button>Add</Button>
-            </div>
-            {/*{cardPacks.map(card => {*/}
-            {/*    return // <div>{card.page}</div>*/}
 
-            {/*})}*/}
+            </div>
+            <div className={show_Table.container}>
+                {cardPacks.map(card => {
+                    return  <PackItam  name={card._id} cardcount={card.cardsCount} update={card.updated}/>
+
+                })}
+            </div>
+
         </div>
     );
 };
--- src/state/reducers/packs.ts
+++ src/state/reducers/packs.ts
@@ -1,9 +1,53 @@
 import {PACKS_ACTIONS, PacksActionsType} from "../actions/packs/types";
 
-export type packsReducerStateType = {}
+export type packsReducerStateType = {
+    pack: PacksType
+}
 
 
-const initState: packsReducerStateType = {}
+const initState: packsReducerStateType = {
+    pack: {
+        cardPacks:[
+            {
+            "_id": "620c180ce12228000414378b",
+            "user_id": "6206a327b34eb500049eef80",
+            "name": "MyNew",
+            "cardsCount": 0,
+           "created": "2022-02-15T21:15:56.261Z",
+            "updated": "2022-02-15T21:32:24.508Z",
+            },
+            {
+                "_id": "620c1af2e122280004143796",
+                "user_id": "61e6cab5914b1230a0a73254",
+                "name": "10",
+                "cardsCount": 0,
+                "created": "2022-02-15T21:28:18.341Z",
+                "updated": "2022-02-15T21:28:18.341Z",
+            },
+            {
+                "_id": "620c1adce122280004143792",
+                "user_id": "61e6cab5914b1230a0a73254",
+                "name": "6",
+                "cardsCount": 0,
+                "created": "2022-02-15T21:27:56.538Z",
+                "updated": "2022-02-15T21:27:56.538Z",
+            },
+            {
+                "_id": "620c1acae122280004143790",
+                "user_id": "61e6cab5914b1230a0a73254",
+                "name": "4",
+                "cardsCount": 0,
+                "created": "2022-02-15T21:27:38.337Z",
+                "updated": "2022-02-15T21:27:38.337Z",
+            },
+        ],
+        cardPacksTotalCount: 4088,
+        maxCardsCount: 103,
+        minCardsCount: 0,
+        page: 1,
+        pageCount: 4,
+    }
+}
 
 export const packsReducer = (state = initState, action: PacksActionsType): packsReducerStateType => {
     switch (action.type) {
