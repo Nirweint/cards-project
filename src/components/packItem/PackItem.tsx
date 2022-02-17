7abcdb558565b4b20f348445449a230e01558c60
MihaThePooh <63107837+MihaThePooh@users.noreply.github.com>
2022-02-17 23:16:57 +0300

    Start PackItem component

---
+++ src/components/packItem/PackItem.tsx
@@ -0,0 +1,39 @@
+import React, {FC} from 'react';
+import {Button} from "../common";
+
+
+type PackItemType = {
+    name: string,
+    cardsCount: number,
+    update: string
+}
+
+
+export const PackItem: FC<PackItemType>= ({name,cardsCount,update}) => {
+
+
+    const addHandler = () => {
+        console.log("addHandler")
+    };
+    const updateHandler = () => {
+        console.log("updateHandler")
+    };
+    const deleteHandler = () => {
+        console.log("deleteHandler")
+    };
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{cardsCount}</td>
+            <td>{update}</td>
+            <td>
+                <Button onClick={addHandler}>Add</Button>
+                <Button onClick={updateHandler}>Update</Button>
+                <Button onClick={deleteHandler}>Delete</Button>
+            </td>
+        </tr>
+    );
+};
+
+export default PackItem;
