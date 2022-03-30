3c46a5f8e52c8a3f1c12756cd14543f899a3ca95
Nirweint <nirweint_1@mail.ru>
2022-03-30 12:48:16 +0300

    config baseUrl change

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const baseURL = 'http://localhost:7542/2.0/';
-// const baseURL = 'https://neko-back.herokuapp.com/2.0';
 const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:7542/2.0/';
 
 export const instance = axios.create({
